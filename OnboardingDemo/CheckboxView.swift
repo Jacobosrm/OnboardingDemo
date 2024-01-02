b1719aa214dcc4ee508db6b2a4d70751b4dda1a4
Jacobo <jacobosramirezm@gmail.com>
2024-01-02 09:28:30 -0500

    commit

---
+++ OnboardingDemo/CheckboxView.swift
@@ -0,0 +1,8 @@
+//
+//  CheckboxView.swift
+//  OnboardingDemo
+//
+//  Created by Jacobo Ramirez on 30/12/23.
+//
+
+import Foundation
---
+++ OnboardingDemo/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  OnboardingDemo
+//
+//  Created by Jacobo Ramirez on 30/12/23.
+//
+
+import Foundation
